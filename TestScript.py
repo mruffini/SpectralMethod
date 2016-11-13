0e2a0890fecb2ce9fbdf9a2ca88eb5765cad3d02
mruffini <matteo.ruffini87@gmail.com>
2016-11-13 16:39:26 +0100

    Added comments

--- TestScript.py
+++ TestScript.py
@@ -24,13 +24,19 @@ c = 3000                    #The number of words per document
 TestSingleTopic = False     #Set to true to test the single topic model, to false to test LDA
 
 if TestSingleTopic:
+    # We generate the random text X, and parameters of the model
     X,M,omega = rand.generate_sample_SingleTopic(N,n,k,c)
+    # We retrieve the tensors we need to learn the model
     M1,M2,M3 = sm.RetrieveTensorsST(X)
+    # We learn the model parameters
     M_tilde,omega_tilde = sm.learn_LVM_Core(M1,M2,M3,k)
     print(np.linalg.norm(M_tilde.dot(np.diag(omega_tilde)).dot(M_tilde.T) - M.dot(np.diag(omega)).dot(M.T) ))
 else:
+    # We generate the random text X, and parameters of the model
     X, M, Alpha = rand.generate_sample_LDA(N, n, k, c)
     Alpha0 = np.sum(Alpha)
+    # We retrieve the tensors we need to learn the model
     M1a,M2a,M3a = sm.RetrieveTensorsLDA(X, Alpha0)
+    # We learn the model parameters
     M_tilde,Alpha_tilde = sm.learn_LVM_Core_LDA(M1a, M2a, M3a, Alpha0, k)
 
